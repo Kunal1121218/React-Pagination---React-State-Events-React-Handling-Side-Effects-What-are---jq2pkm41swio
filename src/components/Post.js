5c6384a4ee2c19712877325cc037fc42611d0d36
Kunal Jadhav <112310504+Kunal1121218@users.noreply.github.com>
2022-12-31 12:18:17 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,7 +2,8 @@ import React from 'react'
 const Post = () => {
     return (
         <div className="post">
-
+            <h3 className='title'>{title}</h3>
+            <p className='body'>{body}</p>
         </div>
     )
 }
