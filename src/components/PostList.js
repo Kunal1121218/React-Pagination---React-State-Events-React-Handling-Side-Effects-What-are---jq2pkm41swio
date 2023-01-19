b75b00e56b8aafc2d1c5a096345b13946d2e6414
Kunal Jadhav <112310504+Kunal1121218@users.noreply.github.com>
2023-01-19 12:32:59 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,15 +1,37 @@
-import React from 'react'
-import Post from './Post'
+import React, { useEffect, useState } from "react";
+import Loader from "./Loader";
+import { PaginationButtonsList } from "./PaginationButtonsList";
+import { Post } from "./Post";
 
-const PostList = ({ posts }) => {
-    
-    return (
-        <div className='post-list'>
-            {
-                posts.map(post => <Post key={post.id} title={post.title} body={post.body} />)
-            }
-        </div>
+const PostList = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      `https://jsonplaceholder.typicode.com/posts?_page=${pageNumber}&_limit=5`
     )
-}
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+        setIsLoading(false);
+      });
+  }, [pageNumber]);
+  return (
+    <>
+      <PaginationButtonsList
+        actPage={pageNumber}
+        setActivePage={setPageNumber}
+      />
+      {isLoading && <Loader />}
+      {!isLoading &&
+        posts.map((post) => {
+          return <Post title={post.title} body={post.body} key={post.title} />;
+        })}
+    </>
+  );
+};
 
-export default PostList 
+export { PostList };
